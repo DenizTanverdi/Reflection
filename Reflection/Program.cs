5f7a6678c540c6bd829e30eb8a40fa3f52edae6e
semvs <semvs@ayvansaray.edu.tr>
2019-01-04 13:51:01 +0300

    Ref

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Data;
 
 namespace Reflection
 {
@@ -10,6 +12,24 @@ namespace Reflection
     {
         static void Main(string[] args)
         {
+            datasetMetotlari();
+            Console.ReadLine();
+        }
+        public static void datasetMetotlari()
+        {
+            Type tip = typeof(DataSet);
+            MethodInfo[] methods = tip.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+            foreach  (MethodInfo m in methods)
+            {
+                Console.WriteLine("Name :"+m.Name);
+                MethodBody mBody = m.GetMethodBody();
+                IList<LocalVariableInfo> lv = mBody.LocalVariables;
+                foreach (var item in lv)
+                {
+                    Console.WriteLine("Type Value" + item.LocalType);
+                }
+               
+            }
         }
     }
 }
