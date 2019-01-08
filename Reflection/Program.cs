cec48cf5e26ff16496104e6a502fdb50bfdd96bf
semvs <semvs@ayvansaray.edu.tr>
2019-01-08 09:23:53 +0300

    Ogrenci eklendi

--- Reflection/Program.cs
+++ Reflection/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using System.Reflection;
-using System.Data;
 
 namespace Reflection
 {
@@ -12,24 +11,78 @@ namespace Reflection
     {
         static void Main(string[] args)
         {
-            datasetMetotlari();
-            Console.ReadLine();
-        }
-        public static void datasetMetotlari()
-        {
-            Type tip = typeof(DataSet);
-            MethodInfo[] methods = tip.GetMethods(BindingFlags.Public | BindingFlags.Instance);
-            foreach  (MethodInfo m in methods)
+
+            stringBilgileriniVer();
+            /*
+             Ogrenci sinifinin public field'larina erişim
+             
+             */
+
+            Type ogrenciTuru = typeof(Ogrenci);
+            FieldInfo[] ogrencialanlari = ogrenciTuru.GetFields(
+                                                         BindingFlags.Public |
+                                                         BindingFlags.Instance);
+
+
+            foreach (FieldInfo ogrenciAlani in ogrencialanlari)
             {
-                Console.WriteLine("Name :"+m.Name);
-                MethodBody mBody = m.GetMethodBody();
-                IList<LocalVariableInfo> lv = mBody.LocalVariables;
-                foreach (var item in lv)
-                {
-                    Console.WriteLine("Type Value" + item.LocalType);
-                }
-               
+                Console.WriteLine("Alan :" + ogrenciAlani.Name);
             }
+
+
+
+
+
+            /*
+             2. Yeni ögrenci Nesnesi
+             */
+
+            Ogrenci ogrenci = new Ogrenci
+            {
+                Adi = "",
+                Soyadi = "ozturk",
+                Bolum = "Matematik"
+
+            };
+
+            if (!ZorunlulukKontrolu.Dogrula(ogrenci))
+            {
+                throw new Exception("Öğrenci bilgileri doğrulamadan geçemedi!");
+            }
+            Console.WriteLine("*****************************\n");
+
+            foreach (FieldInfo ogrenciAlani in ogrencialanlari)
+            {
+                Console.WriteLine("Alan :" + ogrenciAlani.Name);
+                Console.WriteLine("Değer :" + ogrenciAlani.GetValue(ogrenci));
+                Console.WriteLine("******************");
+            }
+
+            Console.ReadKey();
+
+           
+        }
+
+        public static void stringBilgileriniVer()
+        {
+
+            Type tip = typeof(String);
+            MethodInfo[] metotlar = tip.GetMethods(
+                                                         BindingFlags.Public |
+                                                         BindingFlags.Instance);
+
+
+            foreach (MethodInfo metot in metotlar)
+            {
+
+                if (metot.MemberType == MemberTypes.Method)
+                    Console.WriteLine("Metot Adi:" + metot.Name);
+                Console.WriteLine("Metot return type:" + metot.ReturnType);
+
+            }
+
         }
     }
+
+
 }
